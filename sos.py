15af4b7973e10e43299cf59b4e2cd3493633d701
liting <cailiting1993@gmail.com>
2022-09-30 14:00:25 +0300

    Update sos.py

--- sos.py
+++ sos.py
@@ -1,12 +1,27 @@
 import streamlit as st
 import datetime
 import pandas as pd                        
-from pytrends.request import TrendReq
 import plotly.express as px
 import warnings
 warnings.filterwarnings("ignore") 
-pytrend = TrendReq(hl='en-US', tz=120, timeout=(5,10))
 
+from pytrends.request import TrendReq as UTrendReq
+GET_METHOD='get'
+import requests
+
+headers = {
+    'sec-ch-ua': '"Google Chrome";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
+    'Referer': 'https://trends.google.com/',
+    'sec-ch-ua-mobile': '?0',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
+    'sec-ch-ua-platform': '"Windows"',
+}
+
+
+class TrendReq(UTrendReq):
+    def _get_data(self, url, method=GET_METHOD, trim_chars=0, **kwargs):
+        return super()._get_data(url, method=GET_METHOD, trim_chars=trim_chars, headers=headers, **kwargs)
+pytrend = TrendReq()
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: black;'>Google Trends</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: darkgrey;'>Automates search and save the needed keywords for modelling</h2>", unsafe_allow_html=True)
