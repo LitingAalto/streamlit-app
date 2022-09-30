fe893a2aac1c5ac6d1e835b4497e22df5ee99772
liting <cailiting1993@gmail.com>
2022-09-30 14:06:13 +0300

    Update sos.py

--- sos.py
+++ sos.py
@@ -7,7 +7,7 @@ warnings.filterwarnings("ignore")
 
 from pytrends.request import TrendReq
 
-pytrends = TrendReq(hl = 'en-US', tz = 360, timeout = 10, proxies = ['https://34.203.233.13:80', ])
+pytrends = TrendReq(hl='en-US', tz=360, timeout=(10,25), proxies=['https://34.203.233.13:80',], retries=2, backoff_factor=0.1, requests_args={'verify':False})
 st.set_page_config(layout="wide")
 st.markdown("<h1 style='text-align: center; color: black;'>Google Trends</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: darkgrey;'>Automates search and save the needed keywords for modelling</h2>", unsafe_allow_html=True)
